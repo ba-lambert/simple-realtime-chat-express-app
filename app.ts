52b828dda4e22ffe41bbc070dfd80855975d003b
ba-lambert <mugwanezalambert12b@gmail.com>
2024-04-03 23:55:19 +0200

    initial commit

---
+++ app.ts
@@ -0,0 +1,53 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { Server as SocketIOServer } from "socket.io";
+import http from "http";
+import Message from "./model/messages.mode";
+import cors from 'cors';
+import path from "path";
+
+dotenv.config();
+
+const app: Express = express();
+const server = http.createServer(app);
+const io = new SocketIOServer(server);
+const port = process.env.PORT || 3000;
+app.use(cors())
+
+app.use("/socket.io", express.static(path.join(__dirname, "node_modules", "socket.io-client", "dist")));
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/yourdb', {
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
+});
+
+io.on('connection', (socket) => {
+  console.log('user connected', socket.id);
+
+  socket.on('chat message', (data) => {
+    console.log('Received message:', data);
+    const message = new Message({ user: data.user, text: data.message });
+    message.save();
+    console.log(message);
+    
+    io.emit('chat message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.get("/chat", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+server.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
